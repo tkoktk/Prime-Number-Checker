d3567ddc0d611e00ca38b2699c59b1a9002bdda5
Tom Keenan <46943314+tkoktk@users.noreply.github.com>
2024-10-14 19:20:10 +0100

    More progress

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const path = require('path');
 const JSONHandler = require('./jsonHandler');
 const InputChecker = require('./inputChecker');
+const IOHandler = require('./IOHandler');
+
+console.log('Welcome to PrimeChecker!')
 
 //Use env variables to facilitate deployment in different envs
 const filePath = process.env.PRIME_CACHE_PATH || path.join(__dirname, 'jsons/primeNumbers.json');
 const jsonHandler = new JSONHandler(filePath);
 
 const primeNumbersCache = jsonHandler.read();
+console.log('Current Prime Cache: ', primeNumbersCache);
+
+const ioHandler = new IOHandler();
 const inputChecker = new InputChecker();
 
-//inputChecker.checkInput('12321');
+async function getInputFromUser() {
+    const usernameInput = await ioHandler.askQuestion('\nEnter your username: ');
+    let looping = true;
+
+    while (looping) {
+        const numberInput = await ioHandler.askQuestion('\nEnter any numeric sequence: ');
+
+        // Calculations go here
+        ioHandler.print(`You entered: ${numberInput}`);
+        inputChecker.checkInput('12321');
+
+        const nextAction = await ioHandler.askQuestion('Would you like to check another number? \nType "exit" to quit or press "enter" to continue: ');
+
+        if (nextAction.toLowerCase() === 'exit') {
+            looping = false;
+        }
+    }
+    ioHandler.close();
+}
+
+getInputFromUser();
 
 
 
-console.log('Prime Cache: ', primeNumbersCache);
